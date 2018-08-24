ea7e4e77e030e3e91116429477e4d767ebd9cd44
Hugo F <hugo.fok94@gmail.com>
2018-08-24 10:15:01 -0400

    to prod

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ require("dotenv").config();
 const PORT = process.env.PORT || 3001;
 const cors = require("cors");
 
-app.use(express.static(__dirname + "/tone-chat-front/build"));
+app.use(express.static(__dirname + "/front-end/build"));
 app.use(express.json());
 app.use(cors());
 app.use(bodyParser.json());
@@ -16,10 +16,10 @@ app.use(bodyParser.json());
 var ToneAnalyzerV3 = require("watson-developer-cloud/tone-analyzer/v3");
 
 var toneAnalyzer = new ToneAnalyzerV3({
-  version: "2017-09-21",
-  url: "https://gateway.watsonplatform.net/tone-analyzer/api",
-  username: "e1bb33f9-f17e-4ada-a17d-c17be96ee62b",
-  password: "AkLEGacK6I8X"
+  version: process.env.VER,
+  url: process.env.URLLINK,
+  username: process.env.USERN,
+  password: process.env.PW
 });
 
 app.post("/api/tone", function(req, res, next) {
