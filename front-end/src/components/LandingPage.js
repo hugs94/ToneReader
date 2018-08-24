fe69bc9920d98db040618dfee14cec71b0ba92a3
Hugo F <hugo.fok94@gmail.com>
2018-08-24 12:06:14 -0400

    Update while waiting

--- front-end/src/components/LandingPage.js
+++ front-end/src/components/LandingPage.js
@@ -2,13 +2,13 @@ import React from "react";
 
 const LandingPage = () => (
   <div>
-    <h1>ToneChatter</h1>
-    <p>
-      Welcome to ToneChatter. A real-time full stack app built by Hugo Fok.
-      Built with React-Redux-Firebase-WatsonAPI-D3. The App will update colour
-      ranges on the side of the chat room in real-time based on the tonality of
-      the conversation.
-    </p>
+    <h1>ToneChatter/Reader</h1>
+    <h3>
+      Welcome to ToneChatter/Reader. A real-time full stack app built by Hugo
+      Fok. Built with React-Redux-Firebase-WatsonAPI-D3. The App will update
+      colour ranges on the side of the chat room in real-time based on the
+      tonality of the conversation.
+    </h3>
   </div>
 );
 
